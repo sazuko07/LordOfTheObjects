858b493b8f6c7bc30c71067a644dbde941d08f09
sazuko07 <sazu07@gmail.com>
2024-10-09 22:22:04 -0500

    lord of objects latest push

--- src/com/skilldistillery/objectgame/entities/Character.java
+++ src/com/skilldistillery/objectgame/entities/Character.java
@@ -1,7 +1,21 @@
 package com.skilldistillery.objectgame.entities;
 
-	public abstract class Character {
+import java.util.Objects;
+
+public abstract class Character {
 		private String name;
 		private int HP;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getHP() {
+			return HP;
+		}
+		public void setHP(int hP) {
+			HP = hP;
+		}
 }
 
--- src/com/skilldistillery/objectgame/entities/HeroFroZone.java
+++ src/com/skilldistillery/objectgame/entities/HeroFroZone.java
@@ -1,5 +1,35 @@
 package com.skilldistillery.objectgame.entities;
 
+import java.util.Objects;
+
 public class HeroFroZone extends Character{
-			
+		private String name; 
+		private int HP;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getHP() {
+			return HP;
+		}
+		public void setHP(int hP) {
+			HP = hP;
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(HP, name);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			HeroFroZone other = (HeroFroZone) obj;
+			return HP == other.HP && Objects.equals(name, other.name);
+		}
 }

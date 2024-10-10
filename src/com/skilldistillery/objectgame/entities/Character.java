aeca28db012fec952ddb48cdd693acf19e54d51b
Michael <martin.michael1477@gmail.com>
2024-10-09 21:28:13 -0500

    adding character fields

--- src/com/skilldistillery/objectgame/entities/Character.java
+++ src/com/skilldistillery/objectgame/entities/Character.java
@@ -3,5 +3,7 @@ package com.skilldistillery.objectgame.entities;
 	public abstract class Character {
 		private String name;
 		private int HP;
+		private int damage; 
+		private int attackPower; 
 }
 
--- src/com/skilldistillery/objectgame/entities/HeroFroZone.java
+++ src/com/skilldistillery/objectgame/entities/HeroFroZone.java
@@ -1,5 +1,5 @@
 package com.skilldistillery.objectgame.entities;
 
 public class HeroFroZone extends Character{
-			
+	
 }

ab51fbb20d3eee4cfbe9828724efe0a9d10c9ea0
Michael <martin.michael1477@gmail.com>
2024-10-10 22:27:03 -0500

    updated bad guy class

--- src/com/skilldistillery/objectgame/entities/BadGuySyndrome.java
+++ src/com/skilldistillery/objectgame/entities/BadGuySyndrome.java
@@ -3,25 +3,7 @@ package com.skilldistillery.objectgame.entities;
 import java.util.Random;
 
 public class BadGuySyndrome extends Character { 
-	private int treasure;
-	
-	public void loseTreasure() {
-		if (this.treasure > 0) {
-			this.treasure--; 
-		}
-		
-		// reduce damage but don't go under zero
-		int newDamage = this.getDamage() - 1;
-		if(newDamage < 0) {
-			newDamage = 0; 
-		}
-		
-		this.setDamage(newDamage);
-		
-		System.out.println(this.getName() +" loses treasure and now has " + this.treasure + " treasures left!");
-		System.out.println(this.getName() + " damage is now " + this.getDamage());
-	}
-	
+
 	Random hitOrMiss = new Random();
 	boolean hit = hitOrMiss.nextBoolean();
 	
@@ -32,8 +14,6 @@ public class BadGuySyndrome extends Character {
 		if (hit) {
 			System.out.println(this.getName() + " strikes 1 deal of damage and takes your treasure");
 			frozone.takeDamage(1); 
-			// bad guy also loses one treasure
-			loseTreasure(); 
 
 		} else {
 			System.out.println("HA YOU MISS " + this.getName());
